e4ece8a772bb31998f661d1021b6d547fa02f5a5
AntonioCopete <antcopjim@gmail.com>
2023-11-01 21:55:37 +0100

    feat: improve logout client side

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,31 @@
-"use client";
 import Link from "next/link";
 import styles from "./Header.module.scss";
 import { ROUTES } from "../../../constants";
-import { signOut, useSession } from "next-auth/react";
+import { getServerSession } from "next-auth";
+import MenuItem from "../MenuItem/MenuItem";
 
-const Header = () => {
-  const session = useSession();
+const Header = async () => {
+  const session = await getServerSession();
+  console.log(session, "session");
+
+  const MENU_ITEMS = [
+    { title: "Login", route: ROUTES.LOGIN },
+    { title: "Register", route: ROUTES.REGISTER },
+    { title: "Logout", isAuth: session?.user },
+  ];
 
   return (
     <header className={styles.header}>
-      <Link href={ROUTES.LOGIN}>
+      {MENU_ITEMS.map(({ title, route, isAuth }) => (
+        <MenuItem title={title} route={route} isAuth={isAuth} />
+      ))}
+      {/* <Link href={ROUTES.LOGIN}>
         <div className={styles.menuItem}>Login</div>
       </Link>
       <Link href={ROUTES.REGISTER}>
         <div className={styles.menuItem}>Register</div>
       </Link>
-      {session?.data && (
-        <div className={styles.menuItemLogout} onClick={signOut}>
-          Logout
-        </div>
-      )}
+      {session?.user && <div className={styles.menuItemLogout}>Logout</div>} */}
     </header>
   );
 };
