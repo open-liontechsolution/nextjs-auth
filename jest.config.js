00ad86467891751c569b8de85bc012ec210d642f
AntonioCopete <antcopjim@gmail.com>
2023-11-10 02:24:27 +0100

    jest: ignore console.error

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ const createJestConfig = nextJest({
 
 // // Add any custom config to be passed to Jest
 const customJestConfig = {
-	// setupFilesAfterEnv: ['<rootDir>/setup.js'],
+	setupFilesAfterEnv: ['./setup.js'],
 
 	testEnvironment: 'jest-environment-jsdom',
 	collectCoverageFrom: ['src/app/**/*.js', 'src/components/**/*.js'],
